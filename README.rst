1f41e8ece70abcd091e4828c469e3d1f5fec09fb
JoshKing56 <joshuaiking56@gmail.com>
2021-09-23 18:08:46 +0100

    updated readme, gitignore and working on click. Main -> proc_handler

--- README.rst
+++ README.rst
@@ -3,10 +3,37 @@ https://github.com/ezhov-evgeny/webdav-client-python-3
 
 NOTE: WEBDAV3's info function doesn't work, but if you do client.list(path, get_info=True) that gives you what you need
 
+=== 
+Progress
+===
+- Got python to talk to nextcloud
+- Installed pypiserver on proxmox, need to port foward
+- Half way through doing security for pypiserver. References: [1](https://www.jitsejan.com/setting-up-private-pypi-server) [2](https://github.com/pypiserver/pypiserver#quickstart-installation-and-usage) [3](https://medium.com/@christianhettlage/setting-up-a-pypi-server-679f1b55b96) [4](https://www.linode.com/docs/guides/how-to-create-a-private-python-package-repository/) [5](https://johnfraney.ca/posts/2019/05/28/create-publish-python-package-poetry/)
+
+
 ====
 Todo
 ====
-- Multiprocessing
 - CLI application with click
+- Multiprocessing
 - Comparison of cold storage options
-- 
+
+====
+Ideas
+====
+[Progress bar](https://stackoverflow.com/questions/32080382/using-click-progressbar-with-multiprocessing-in-python)
+
+ # print(client.get_size(client.list()[1]))
+    # for directory in root_directory:
+    #     if current_space_usage + max_space_usage 
+
+        # TODO: change this to multiprocessing maybe, or just a vectorized loop 
+        # if there is more than x amount of ram available
+        # thread = threading.Thread(target=archiveSubdirectory, args=(directory))
+
+# create temp folder for downloading
+# for each file in nextcloud:
+#   if archive with name nc.ARCHIVE_NAME doesn't exist:
+#       create new archive and add file
+#   else:
+#       add file to archive
--- main.py
+++ main.py
@@ -1,66 +1,14 @@
 import click
-from nextcloud_archiver.webdavclient import WebDavClient
-from nextcloud_archiver.compression import archiveSubdirectory
-from nextcloud_archiver.cacheallocator import CacheAllocator
-import configuration.arguments as args
-from webdav3.client import Client
-import multiprocessing
-
-def list_directories_by_depth(root_list: list, webdav: Client, depth: int = 0) -> list:
-    """
-    This method returns a list of all paths that need to be processed as an individual file group.
-    Directories are determined by whether the file ends in a "/" character
-    Note: the pass
-
-    :param webdav: The webdav client object
-    :param depth: Indicates the depth of file crawling
-    """
-    filelist = list()
-    root_list.pop(0) # First item is always the name of the current directory
-    print(root_list)
-    while depth > 0:
-        for file in root_list:
-            print("\nFile in root: " + file)
-            if file[-1] == "/": # All directories end in "/", while files don't
-                sublist = map(lambda x: file + x, webdav.list(file)[1:])
-                print(list(sublist))
-                filelist += sublist
-            else:
-                filelist += file
-        depth -= 1
-    return filelist
-
-def main():
-    depth = -1
-    end_archive = None
-    threads = list()
-    # client = Client(args.WEBDAV_OPTIONS)
-    client = WebDavClient()
-    root_directory = client.list()[1:]
-    current_space_usage = 0 #in bytes
-    max_space_usage = 1000000000 # 1000 Gb 
-    print(root_directory[8] + "\n\n")
-    print(client.list(root_directory[0], get_info=True))
-
-    # print(client.get_size(client.list()[1]))
-    # for directory in root_directory:
-    #     if current_space_usage + max_space_usage 
-
-        # TODO: change this to multiprocessing maybe, or just a vectorized loop 
-        # if there is more than x amount of ram available
-        # thread = threading.Thread(target=archiveSubdirectory, args=(directory))
 
 
-    
-    #   start thread with
+@click.command()
+@click.option("-f", "--config", default="config/arguments.py", help="Path to the configuration file. Defaults to config/arguments.py")
+def main(config):
+    '''
+    Utility for pulling data from a WebDav server, compressing data, and moving snapshot to cold storage
+    '''
+    print(config)
 
-    
 
 if __name__ == "__main__":
     main()
-# create temp folder for downloading
-# for each file in nextcloud:
-#   if archive with name nc.ARCHIVE_NAME doesn't exist:
-#       create new archive and add file
-#   else:
-#       add file to archive
